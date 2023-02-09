7fc44795cac20965bf0ac333a4310259c8daa04f
Francisco-Fortes <fortesfco@gmail.com>
2023-02-09 23:38:10 +0100

    auth folder added
    middlewares & tools

---
+++ src/auth/basicAuth.js
@@ -0,0 +1,25 @@
+import createHttpError from "http-errors";
+import atob from "atob";
+import usersModel from "../../api/users/model.js";
+
+export const basicAuthMiddleware = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    next(
+      createHttpError(
+        401,
+        "Please provide credentials in the Authorization header!"
+      )
+    );
+  } else {
+    const encodedCredentials = req.headers.authorization.split(" ")[1];
+    const credentials = atob(encodedCredentials);
+    const [email, password] = credentials.split(":");
+    const user = await usersModel.checkCredentials(email, password);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      next(createHttpError(401, "Credentials not valid!"));
+    }
+  }
+};
